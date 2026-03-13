1db7e762b5d4e31291cb761afb72cc8ab44c4928
ngambasty-tech <ngambasty@gmail.com>
2026-03-13 20:26:31 +0100

    functions as objects, template literals, primitive vs reference values

---
+++ jsbehind-the-scene.js
@@ -0,0 +1,7 @@
+const sports = ['football','formula 1']; //what is being stored is not the content of sports but a pointer
+sports.push('tennis');// pointer doesnt change
+console.log(sports)
+const number = 12; //the actual value is stored 
+//primitive values: numbers,booleans,strings etc
+//reference valuse: Objects, arrays
+//both stored in diff memories
